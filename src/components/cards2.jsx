40918876611c85face77ca27ef6984f20bad252c
iancabak <99687250+iancabak@users.noreply.github.com>
2022-06-20 14:53:10 -0300

    Add files via upload

---
+++ src/components/cards2.jsx
@@ -0,0 +1,9 @@
+function Cartas() {
+    return (
+        <div className="wrapper">
+            <h1>hola</h1>
+        </div>
+    )
+}
+
+export default Cartas
---
+++ src/components/intel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Intel = () => {
+    return (
+        <div className='horizontal'>
+            <div className='wrapper3'>
+            <h2 className='inteltitulo'>En <span>Dark Horse</span>  utilizamos el microprocesador mas moderno:</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta, quod sapiente quia adipisci. Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta, quod sapiente quia adipisci. Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta, quod sapiente quia adipisci. </p>
+            </div>
+            <div className='imagen'>
+            <a href='https://ark.intel.com/content/www/es/es/ark/products/197384/intel-core-i711375h-processor-12m-cache-up-to-5-00-ghz-with-ipu.html'>
+                <img className='intel' src='https://br.atsit.in/es/wp-content/uploads/2021/08/la-cpu-intel-core-i7-de-12a-generacion-supera-la-serie-ryzen-7-5000.jpg'></img>
+            </a>
+            </div>
+            
+        </div>)
+        
+}
+
+export default Intel
